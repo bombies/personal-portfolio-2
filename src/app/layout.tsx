eda7f40b5c2ebaec6758c0cd67ccade10701159b
bombies <xjamesdzn@gmail.com>
2024-04-09 09:42:03 -0500

    Add Google Analytics tagging

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
-import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
-import './globals.scss'
-import clsx from 'clsx'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.scss";
+import clsx from "clsx";
 import NavBar from "@/components/NavBar";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
-const inter = Inter({subsets: ['latin'], variable: '--font-inter'})
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
-    title: 'Ajani Green - Full Stack Developer',
-    description: 'The portfolio of Ajani Green',
-}
+    title: "Ajani Green - Full Stack Developer",
+    description: "The portfolio of Ajani Green",
+};
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+    children,
+}: {
+    children: React.ReactNode;
 }) {
     return (
         <html lang="en">
-        <body className={clsx(inter.className)}>
-        <NavBar/>
-        {children}
-        </body>
+            <body className={clsx(inter.className)}>
+                <NavBar />
+                {children}
+            </body>
+            <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ""} />
         </html>
-    )
+    );
 }
